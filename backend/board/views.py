bd847d0db457335ad51821f7a2fa6c9df1e229ea
dooby99 <102219869+dooby99@users.noreply.github.com>
2025-04-28 20:10:05 +0900

    Update views.py

--- backend/board/views.py
+++ backend/board/views.py
@@ -7,6 +7,8 @@ class BoardListCreateView(generics.ListCreateAPIView):
     queryset = Board.objects.all().order_by('-created_at')
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class BoardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
